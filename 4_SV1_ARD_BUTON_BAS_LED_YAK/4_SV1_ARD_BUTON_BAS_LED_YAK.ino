78b1df5ebe1f2ebb90de90ae74bf4c2a1fdf06d9
Pinova Elektronik Mühendislik <66210166+Pinova-hub@users.noreply.github.com>
2021-02-24 14:22:04 +0300

    Update 4_SV1_ARD_BUTON_BAS_LED_YAK.ino

--- 4_SV1_ARD_BUTON_BAS_LED_YAK/4_SV1_ARD_BUTON_BAS_LED_YAK.ino
+++ 4_SV1_ARD_BUTON_BAS_LED_YAK/4_SV1_ARD_BUTON_BAS_LED_YAK.ino
@@ -1,29 +1,29 @@
-#define LED 22  
-#define BUTON  5
+#define LED 23                                  //LED değişkeni ve bağlı olduğu pin tanımlanır
+#define BUTON  7                                //Buton değişkeni ve bağlı olduğu pin tanımlanır (0.pini kullanmayınız)
    
-int Buton_durumu=1;   
+int Buton_durumu=1;                             //Buton_durumu tanımlanır ve butonun başlangıçtaki dijital durumu değişkene atanır(Butonlar pull-up bağlıolduğundan başlangıçtaki dijital udurmları 1'dir. Pull-down buton kullanılması durumunda başlangıçtaki dijital durumu 0 olarak girilmelidir)
 
 void setup() {
   
- pinMode(BUTON,INPUT);
- pinMode(LED,OUTPUT);
+ pinMode(BUTON,INPUT);                          //Butona giriş verilir
+ pinMode(LED,OUTPUT);                           //LED'e çıkış verilir
 
 }
 void loop() {
   
-Buton_durumu = digitalRead (BUTON);
+Buton_durumu = digitalRead (BUTON);             //Butona basılı olup olmadığı dijital olarak okunarak kontrol edilir ve durumu Buton_durumu değişkenine atanır
  
-    if (Buton_durumu == 0)
+    if (Buton_durumu == 0)                      //Buton basılıysa
     
     {
-      digitalWrite(LED,HIGH);
+      digitalWrite(LED,HIGH);                   //LED'e dijital 1 (5V) verilir, LED yakılır
     }
     
     
-    else
+    else                                        //Değilse
     
     {
-      digitalWrite(LED,LOW);
+      digitalWrite(LED,LOW);                    //LED'e dijital 0 (0V) verilir, LED söndürülür
     }
     
 }
